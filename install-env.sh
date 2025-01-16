1c249a941e433d3bd26be309d00f9e0068cd1060
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2025-01-15 22:20:07 -0300

    adding install deps script and restore script
    install-env script installs dependencies for the environment
    restore script copies the dotfiles back to the places they belong

---
+++ install-env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+# Script to download common tools used to development in a endeavour os environment
+#
+
+sudo pacman -S git nitrogen asdf-vm zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+chsh -s $(which zsh)
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+asdf plugin add nodejs
+asdf install nodejs latest
+
+# Installing wallpaper
+if [! -d $HOME/Imagens/wallpapers ]; then
+  mkdir -p $HOME/Imagens/wallpapers
+fi
+
+cp ./assets/wp.jpg $HOME/Imagens/wallpapers/wp.jpg
+
+nitrogen --set-auto $HOME/Imagens/wallpapers/wp.jpg >/dev/null 2>/dev/null
+
+./restore.sh
---
+++ restore.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#
+# This file restore the config dot files that where versioned here
+#
+
+cp ./.config/i3/* $HOME/.config/i3/
+cp ./.alacritty.toml $HOME/.alacritty.toml
